f8068b859cc5bb85c816f1eaa89d642532fdb45f
Ellen Spertus <spertus@mills.edu>
2020-08-31 21:32:36 -0700

    Add failing tests of getUserName

--- src/edu/mills/cs180a/EmailAddressParserTest.java
+++ src/edu/mills/cs180a/EmailAddressParserTest.java
@@ -3,15 +3,15 @@ package edu.mills.cs180a;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static edu.mills.cs180a.EmailAddressParser.isValid;
+import static edu.mills.cs180a.EmailAddressParser.*;
 
 class EmailAddressParserTest {
 
 	@Test
-	void isValidRejectsNull() {
 		assertFalse(isValid(null),
 				"isValid() should reject null");
 	}
@@ -27,5 +27,23 @@ class EmailAddressParserTest {
 	void isValidRejectsMalformedEmails(String candidate) {
 		assertFalse(isValid(candidate),
 				"isValid() should reject \"" + candidate + "\"");
+	}
+	
+	@Test
+	void getUserNameWithValidEmails() {
+		assertEquals("susan", getUserName("susan@mills.edu"));
+	}
+	
+	@Test
+	void getUserNameRejectsMalformedEmails() {
+		assertThrows(IllegalArgumentException.class,
+				new Executable() {
+					@Override
+					public void execute() {
+						getUserName(null);
+					}
+		});
+		assertThrows(IllegalArgumentException.class,
+				() -> getUserName(null));
 	}
 }
